c2b2a563fa29299e9400484b588166914651fdec
TryingToCode <TryingToCode@users.noreply.github.com>
2023-06-13 22:21:58 +0100

    Add files via upload

---
+++ MusicGigsEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhotoPitLive
+{
+    public class MusicGigsEntity
+    {
+        public long GigDate { get; set; }
+        public DateTime GigDateDisplay { get; set; }
+        public string GigDateShortDisplay { get; set; }
+        public string GigMonth { get { return GigDateDisplay.Month.ToString();} }
+        public string GigMonth2 { get { return GigDateDisplay.ToString("MMMM"); } }
+        public string GigMonth3 { get { return GigDateDisplay.ToString("yyyy-MM-dd") + "_" + Guid.NewGuid(); } }
+        public string Artist { get; set; } = default!;
+        public long Venue { get; set; }
+        public string VenueDisplay { get; set;  }
+        public string Url { get; set; }
+    }
+
+    public class VenueList
+    {
+        public string VenueDisplay { get; set; }
+        public int CountOfGigs { get; set; }
+        public string NormalisedName { get; set; }
+    }
+}
---
+++ MusicGigsModule.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using NetFabric.Hyperlinq;
+using Statiq.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoPitLive
+{
+    public class MusicGigsModule : Module
+    {
+        protected override async Task<IEnumerable<IDocument>> ExecuteContextAsync(IExecutionContext context)
+        {
+            var data = new MusicDataFetch().GetContentFromContentful<MusicGigsEntity>("gigs");
+
+            List<IDocument> outputs = new List<IDocument>();
+
+            foreach (var item in data)
+            {
+                var StringObject = new Dictionary<string, object>
+                {
+                    { nameof(item.Artist), item.Artist },
+                    { nameof(item.Url), item.Url },
+                    { nameof(item.GigDateDisplay), item.GigDateDisplay },
+                    { nameof(item.GigMonth3), item.GigMonth3 },
+                    { nameof(item.VenueDisplay), item.VenueDisplay },
+                    { nameof(item.Venue), item.Venue },
+                    { nameof(item.GigDateShortDisplay), item.GigDateShortDisplay },
+                };
+
+                var doc = context.CreateDocument(item.Artist, StringObject, item.VenueDisplay);
+
+                outputs.Add(doc);
+            }
+
+            return outputs;
+        }
+    }
+}
