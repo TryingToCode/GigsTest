e922e8a6b91f7f4e0c605b1821db071662e0e63d
TryingToCode <TryingToCode@users.noreply.github.com>
2023-06-13 22:27:17 +0100

    Add files via upload

---
+++ MusicGigsPipeline.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Markdown;
+using Statiq.Yaml;
+using System;
+using System.Text;
+
+namespace PhotoPitLive
+{
+    public class MusicGigsPipeline : Pipeline
+    {
+        public MusicGigsPipeline()
+        {
+            DependencyOf.Add(nameof(Statiq.Web.Pipelines.Inputs));
+            DependencyOf.Add(nameof(Statiq.Web.Pipelines.Content));
+
+            InputModules = new ModuleList{
+              new MusicGigsModule(),
+            };
+
+            ProcessModules = new ModuleList {
+                new SetContent(Config.FromDocument(doc => doc.GetString("Artist")), MediaTypes.Markdown),
+                new RenderMarkdown(),
+                new SetDestination(Config.FromDocument(doc => new NormalizedPath($"gigs2/{doc.GetString("GigMonth3")}.html").OptimizeFileName()))};
+
+            OutputModules = new ModuleList {
+              new WriteFiles()
+          };
+        }
+    }
+}
